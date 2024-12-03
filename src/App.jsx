b91dc7df323442a438521d62f8bf555fdd7ec1de
Nahid Hasan <nahidn228@gmail.com>
2024-12-03 09:21:52 +0600

    env variable added for firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar></Navbar>
